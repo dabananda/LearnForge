0713d433b81e7ec8cae9a1073457cbc6c1954ca4
dabananda <dabananda.dev@gmail.com>
2026-06-03 10:22:16 +0600

    feat: login feature implemented with localstorage token saveing

--- frontend/angular/angular-media/src/app/features/auth/services/Auth/auth-state.service.ts
+++ frontend/angular/angular-media/src/app/features/auth/services/Auth/auth-state.service.ts
@@ -5,9 +5,10 @@ import { computed, Injectable, signal } from '@angular/core';
 })
 export class AuthStateService {
   readonly token = signal<string | null>(localStorage.getItem("access_token"));
-  readonly isAuthenticated = computed(() => !!this.token);
+  readonly isAuthenticated = computed(() => !!this.token());
 
   setToken(token: string) {
+    localStorage.setItem("access_token", token);
     this.token.set(token);
   }
 
--- frontend/angular/angular-media/src/app/features/auth/services/Auth/auth.service.ts
+++ frontend/angular/angular-media/src/app/features/auth/services/Auth/auth.service.ts
@@ -3,18 +3,20 @@ import { HttpClient } from '@angular/common/http';
 import { TokenService } from '../Token/token.service';
 import { LoginRequest } from '../../models/login-request';
 import { tap } from 'rxjs';
-import { ApiResponse } from '../../models/api-response.model';
+import { ApiResponse } from '../../../../shared/models/api-response.model';
+import { AuthStateService } from './auth-state.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
   private readonly http = inject(HttpClient);
-  private readonly tokenService = inject(TokenService);
+  private readonly authStateService = inject(AuthStateService);
   private readonly loginUrl = 'https://localhost:7100/api/v1/users/login';
 
   login(request: LoginRequest) {
-    return this.http.post<ApiResponse<string>>(this.loginUrl, request)
-      .pipe(tap(res => this.tokenService.setToken(<string>res.data)))
+    return this.http
+      .post<ApiResponse<string>>(this.loginUrl, request)
+      .pipe(tap((res) => this.authStateService.setToken(<string>res.data)));
   }
 }
