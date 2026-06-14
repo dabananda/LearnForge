8e9a5bccd5e9c074b3c6078903af3a71f98eb6a9
dabananda <dabananda.dev@gmail.com>
2026-06-14 11:16:01 +0600

    feat: Add Category module with API, DTOs, and scaffolding

---
+++ backend/InvoiceGenerator/InvoiceGenerator.Modules.Product.Handlers/DependencyCollection.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace InvoiceGenerator.Modules.Product.Handlers
+{
+    public static class DependencyCollection
+    {
+        public static IServiceCollection AddProduct(this IServiceCollection services)
+        {
+            
+            return services;
+        }
+    }
+}
